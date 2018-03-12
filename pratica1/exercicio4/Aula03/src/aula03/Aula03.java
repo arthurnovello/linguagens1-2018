4693a9767e33d182e98a7fc46d09ddb8741b7970
Arthur Novello <novelloarthur@gmail.com>
2018-03-12 12:54:17 -0300

    Aula 12.03

---
+++ pratica1/exercicio4/Aula03/src/aula03/Aula03.java
@@ -0,0 +1,15 @@
+package aula03;
+
+/**
+ *
+ * @author 14.03329-0
+ *         Arthur Novello
+ */
+public class Aula03 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Salve, Netbeans");
+    }
+    
+}
